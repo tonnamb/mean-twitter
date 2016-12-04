a9e8cb0c6b650e4268fa0031fb96537c7a676b25
Tonnam Balankura <tonnamb@Tonnams-MacBook-Pro.local>
2016-12-03 19:15:06 -0500

    Finish angular

--- app/js/chirpApp.js
+++ app/js/chirpApp.js
@@ -9,3 +9,16 @@ app.controller('mainController', function($scope) {
 		$scope.newPost = {created_by: '', text: '', created_at: ''};
 	};
 });
+
+app.controller('authController', function($scope) {
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
+
+	$scope.login = function() {
+		$scope.error_message = "Login request for " + $scope.user.username;
+	};
+
+	$scope.register = function() {
+		$scope.error_message = "Register request for " + $scope.user.username;
+	};
+});
